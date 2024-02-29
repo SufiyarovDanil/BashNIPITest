0d3ce900f8364c18323237c9cbf456b72cef76e7
SufiyarovDanil <danil-sufiyarov@mail.ru>
2024-03-01 00:00:40 +0500

    переписал бд с нуля

--- src/resources/sql/init.sql
+++ src/resources/sql/init.sql
@@ -1,21 +1,27 @@
-CREATE TYPE point3D AS
-(
-	x DOUBLE PRECISION,
-	y DOUBLE PRECISION,
-	z DOUBLE PRECISION
-);
+-- Database: bnipi_v2
+
+DROP DATABASE IF EXISTS bnipi_v2;
+
+CREATE DATABASE bnipi_v2
+    WITH
+    OWNER = postgres
+    ENCODING = 'UTF8'
+    LC_COLLATE = 'Russian_Russia.1251'
+    LC_CTYPE = 'Russian_Russia.1251'
+    TABLESPACE = pg_default
+    CONNECTION LIMIT = -1
+    IS_TEMPLATE = False;
+
 
 -- Table: public.well
 
--- DROP TABLE IF EXISTS public.well;
+DROP TABLE IF EXISTS public.well;
 
 CREATE TABLE IF NOT EXISTS public.well
 (
     pk_id uuid NOT NULL,
     name character varying(64) COLLATE pg_catalog."default" NOT NULL,
     head point NOT NULL,
-    measured_depth double precision[] NOT NULL,
-    trajectory point3d[] NOT NULL,
     CONSTRAINT well_pkey PRIMARY KEY (pk_id),
     CONSTRAINT well_name_key UNIQUE (name)
 )
@@ -23,4 +29,29 @@ CREATE TABLE IF NOT EXISTS public.well
 TABLESPACE pg_default;
 
 ALTER TABLE IF EXISTS public.well
+    OWNER to postgres;
+
+
+-- Table: public.trajectory
+
+DROP TABLE IF EXISTS public.trajectory;
+
+CREATE TABLE IF NOT EXISTS public.trajectory
+(
+    pk_id bigint NOT NULL DEFAULT nextval('trajectory_pk_id_seq'::regclass),
+    md double precision NOT NULL,
+    x double precision NOT NULL,
+    y double precision NOT NULL,
+    z double precision NOT NULL,
+    fk_well_id uuid NOT NULL,
+    CONSTRAINT trajectory_pkey PRIMARY KEY (pk_id),
+    CONSTRAINT trajectory_fk_well_id_fkey FOREIGN KEY (fk_well_id)
+        REFERENCES public.well (pk_id) MATCH SIMPLE
+        ON UPDATE CASCADE
+        ON DELETE CASCADE
+)
+
+TABLESPACE pg_default;
+
+ALTER TABLE IF EXISTS public.trajectory
     OWNER to postgres;
