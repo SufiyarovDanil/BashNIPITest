ccf6c28c0d218f9c7abb9b5b9c442ba480d2918b
SufiyarovDanil <danil-sufiyarov@mail.ru>
2024-03-07 23:09:27 +0500

    сделал класс Database оберткой для asyncpg

--- src/database.py
+++ src/database.py
@@ -1,17 +1,12 @@
-import asyncpg
-from asyncpg.connection import Connection
+from typing import Any
 
-from config import (
-    DB_NAME,
-    DB_HOST,
-    DB_PASS,
-    DB_PORT,
-    DB_USER
-)
+import asyncpg as apg
+
+from config import DB_NAME, DB_HOST, DB_PASS, DB_PORT, DB_USER
 
 
-async def make_apg_connection() -> Connection:
-    return await asyncpg.connect(
+async def make_apg_connection() -> apg.Connection:
+    return await apg.connect(
         host=DB_HOST,
         port=DB_PORT,
         user=DB_USER,
@@ -24,9 +19,9 @@ class Database:
     def __init__(self):
         self._connection_pool = None
     
-    async def get_connection_pool(self) -> asyncpg.Pool:
+    async def get_connection_pool(self) -> apg.Pool:
         if not self._connection_pool:
-            self._connection_pool = await asyncpg.create_pool(
+            self._connection_pool = await apg.create_pool(
                 host=DB_HOST,
                 port=DB_PORT,
                 user=DB_USER,
@@ -37,6 +32,51 @@ class Database:
             )
         
         return self._connection_pool
+
+    async def execute(self, query: str, *args) -> apg.Record:
+        pool: apg.Pool = await self.get_connection_pool()
+
+        async with pool.acquire() as conn:
+            async with conn.transaction():
+                result: apg.Record = await conn.execute(query, *args)
+
+        return result
+
+    async def executemany(self, query: str, *args) -> apg.Record:
+        pool: apg.Pool = await self.get_connection_pool()
+
+        async with pool.acquire() as conn:
+            async with conn.transaction():
+                result: apg.Record = await conn.executemany(query, *args)
+
+        return result
+
+    async def fetch(self, query: str, *args) -> apg.Record:
+        pool: apg.Pool = await self.get_connection_pool()
+
+        async with pool.acquire() as conn:
+            async with conn.transaction():
+                result: apg.Record = await conn.fetch(query, *args)
+
+        return result
+
+    async def fetch_row(self, query: str, *args) -> apg.Record:
+        pool: apg.Pool = await self.get_connection_pool()
+
+        async with pool.acquire() as conn:
+            async with conn.transaction():
+                result: apg.Record = await conn.fetchrow(query, *args)
+
+        return result
+
+    async def fetch_val(self, query: str, *args) -> Any:
+        pool: apg.Pool = await self.get_connection_pool()
+
+        async with pool.acquire() as conn:
+            async with conn.transaction():
+                result: Any = await conn.fetchval(query, *args)
+
+        return result
 
 
 db_instance: Database = Database()
