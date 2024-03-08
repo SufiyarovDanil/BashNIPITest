e53ac3e367fa19940af3dc210c1c9b05854c3780
SufiyarovDanil <danil-sufiyarov@mail.ru>
2024-03-08 20:12:14 +0500

    добавил документацию для swagger

--- src/schemas/well.py
+++ src/schemas/well.py
@@ -3,18 +3,40 @@
 
 """
 
-from uuid import uuid4
-
 from pydantic import BaseModel, Field, UUID4, computed_field
 from typing import Any
 
 
 class WellSchema(BaseModel):
-    method: str
+    method: str = Field(default='well.method')
     params: Any
 
 
 class WellCreateSchema(WellSchema):
+    """
+    Тело запроса для создания скважины.
+
+    Параметры:
+
+    name: имя скважины;
+
+    head: координаты устья скважины (x, y);
+
+    MD: список из уровней глубины;
+
+    X, Y, Z: три отдельных списка с координатами точек, представляющих
+    саму скважину.
+
+    Аргументы "MD", "X", "Y" и "Z" являются списками из чисел с
+    плавающей точкой.
+    То есть, элементы X[n], Y[n] и Z[n] представляют координаты
+    скважины на глубине MD[n].
+
+    Поэтому аргументы "MD", "X", "Y" и "Z" должны быть одинаковой
+    длины!
+
+    """
+
     class WellCreateParamsSchema(BaseModel):
         name: str = Field(default='well_name', min_length=1)
         head: tuple[float, float] = Field(default=(0.0, 0.0),
@@ -28,29 +50,74 @@ class WellCreateSchema(WellSchema):
 
 
 class WellRemoveSchema(WellSchema):
+    """
+    Тело запроса для удаления скважины.
+
+    Параметры:
+
+    uuid: идентификатор скважины, которую вы хотите удалить.
+
+    """
+
     class WellRemoveParamsSchema(BaseModel):
-        uuid: UUID4 = Field(default=uuid4())
+        uuid: UUID4 = Field()
     
     params: WellRemoveParamsSchema
 
 
 class WellGetSchema(WellSchema):
+    """
+    Тело запроса для получения данных о скважине.
+
+    Параметры:
+
+    uuid: идентификатор скважины;
+
+    return_trajectory: при значении true вместе с основной информацией
+    возвращает координаты точек вместе с "MD".
+
+    
+    """
     class WellGetParamsSchema(BaseModel):
-        uuid: UUID4 = Field(default=uuid4())
+        uuid: UUID4 = Field()
         return_trajectory: bool = Field(default=False)
     
     params: WellGetParamsSchema
 
 
 class WellAtSchema(WellSchema):
-    class WellGetParamsSchema(BaseModel):
-        uuid: UUID4 = Field(default=uuid4())
+    """
+    Тело запроса для получения координат скважины на определенной
+    глубине.
+
+    Параметры:
+
+    uuid: идентификатор скважины;
+
+    MD: уровень глубины скважины.
+
+    """
+    
+    class WellAtParamsSchema(BaseModel):
+        uuid: UUID4 = Field()
         MD: float = Field()
     
-    params: WellGetParamsSchema
+    params: WellAtParamsSchema
 
 
 class WellOutputSchema(BaseModel):
+    """
+    Является основным форматом ответа API.
+
+    Поля:
+
+    data: содержит полученные данные;
+
+    error: содержит информацию об ошибке (имеет значение null, когда
+    запрос успешен).
+
+    """
+
     data: dict[str, Any] | None = Field(default=None)
 
     def __init__(self,
