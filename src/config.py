036034aa5dfbe5d5cab9e051f355b5ba3ffefddd
SufiyarovDanil <danil-sufiyarov@mail.ru>
2024-03-09 20:24:39 +0500

    убрал лишние переменные

--- src/config.py
+++ src/config.py
@@ -5,19 +5,13 @@
 
 import os
 
-from dotenv import load_dotenv
+# from dotenv import load_dotenv
 
 
-load_dotenv()
+# load_dotenv()
 
 DB_HOST: str = os.environ.get('DB_HOST')
 DB_PORT: str = os.environ.get('DB_PORT')
 DB_USER: str = os.environ.get('DB_USER')
 DB_PASS: str = os.environ.get('DB_PASS')
 DB_NAME: str = os.environ.get('DB_NAME')
-
-DB_TEST_HOST: str = os.environ.get('DB_TEST_HOST')
-DB_TEST_PORT: str = os.environ.get('DB_TEST_PORT')
-DB_TEST_USER: str = os.environ.get('DB_TEST_USER')
-DB_TEST_PASS: str = os.environ.get('DB_TEST_PASS')
-DB_TEST_NAME: str = os.environ.get('DB_TEST_NAME')
